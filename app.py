418bfc31f0549a41f137133cfca00e9c56376046
it-sean-security <sean.dai@kkday.com>
2021-11-29 17:32:47 +0800

    change API post to get

--- app.py
+++ app.py
@@ -143,39 +143,39 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
 
-@app.post("/get_google_news")
+@app.get("/get_google_news")
 async def get_google_news(item:News):
-    return news.get_google_news(item.keyword)
-@app.post("/get_udn_news")
+    return news().get_google_news(item.keyword)
+@app.get("/get_udn_news")
 async def get_udn_news(item:News):
-    return news.get_udn_news(item.keyword)
-@app.post("/get_apple_news")
+    return news().get_udn_news(item.keyword)
+@app.get("/get_apple_news")
 async def get_apple_news(item:News):
-    return news.get_apple_news(item.keyword)
-@app.post("/get_setn_news")
+    return news().get_apple_news(item.keyword)
+@app.get("/get_setn_news")
 async def get_setn_news(item:News):
-    return news.get_setn_news(item.keyword)
-@app.post("/get_ettoday_news")
+    return news().get_setn_news(item.keyword)
+@app.get("/get_ettoday_news")
 async def get_ettoday_news(item:News):
-    return news.get_ettoday_news(item.keyword)
-@app.post("/get_TVBS_news")
+    return news().get_ettoday_news(item.keyword)
+@app.get("/get_TVBS_news")
 async def get_TVBS_news(item:News):
-    return news.get_TVBS_news(item.keyword)
-@app.post("/get_china_news")
+    return news().get_TVBS_news(item.keyword)
+@app.get("/get_china_news")
 async def get_china_news(item:News):
-    return news.get_china_news(item.keyword)
-@app.post("/get_storm_news")
+    return news().get_china_news(item.keyword)
+@app.get("/get_storm_news")
 async def get_storm_news(item:News):
-    return news.get_storm_news(item.keyword)
-@app.post("/get_ttv_news")
+    return news().get_storm_news(item.keyword)
+@app.get("/get_ttv_news")
 async def get_ttv_news(item:News):
-    return news.get_ttv_news(item.keyword)
-@app.post("/get_ftv_news")
+    return news().get_ttv_news(item.keyword)
+@app.get("/get_ftv_news")
 async def get_ftv_news(item:News):
-    return news.get_ftv_news(item.keyword)
-@app.post("/get_cna_news")
+    return news().get_ftv_news(item.keyword)
+@app.get("/get_cna_news")
 async def get_cna_news(item:News):
-    return news.get_cna_news(item.keyword)
+    return news().get_cna_news(item.keyword)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
