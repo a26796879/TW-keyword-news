e11b18dca56199cee1bd13e34d2570ae78feb884
a26796879 <a26796879@gmail.com>
2024-05-23 21:27:13 +0800

    因應蘋果新聞改版調整

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ class NewsCrawler:
 
     async def get_udn_news(self, async_session, keyword):
         ''' get udn news by crawling'''
-        udn_url = 'https://udn.com/api/more?page=0&id=search:' + \
-            urllib.parse.quote_plus(keyword) + '&channelId=2&type=searchword'
+        udn_url = f'https://udn.com/api/more?page=0&id=search:\
+            {urllib.parse.quote_plus(keyword)}&channelId=2&type=searchword'
         res = await async_session.get(url=udn_url, headers=self.headers)
         news = res.json()['lists']
         results = []
@@ -63,7 +63,6 @@ class NewsCrawler:
             title = news[value[0]]['title']
             url = news[value[0]]['titleLink']
             publisher = 'udn聯合新聞網'
-            #image = news[i]['url']
             expect_time = datetime.today() - timedelta(hours=8)
             if published_date >= expect_time:
                 results.append({
@@ -78,43 +77,41 @@ class NewsCrawler:
 
     async def get_apple_news(self, async_session, keyword):
         ''' get apple news by crawling'''
-        apply_url = 'https://tw.appledaily.com/search/' + \
-            urllib.parse.quote_plus(keyword)
+        apply_url = f'https://tw.nextapple.com/search/{urllib.parse.quote_plus(keyword)}/1'
         res = await async_session.get(url=apply_url, headers=self.headers)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        publish = soup.select('div.timestamp')
+        all_page_soup = BeautifulSoup(res.text, 'html.parser')
+        main_content = all_page_soup.find_all("div", id='main-content')
+        soup = BeautifulSoup(str(main_content), 'html.parser')
+        publish = soup.find_all("div", class_='post-inner')
         results = []
-        for i in range(len(publish)):
-            date_string = soup.select('div.timestamp')[i].text
-            date_format = "出版時間：%Y/%m/%d %H:%M"
+        for i, each_news in enumerate(publish):
+            title = each_news.a.text.replace('\u3000', ' '),  # 將全形space取代為半形space
+            news_url = each_news.a.get('href')
+            date_string = each_news.time.text
+            date_format = "%Y/%m/%d %H:%M"
             published_date = datetime.strptime(date_string, date_format)
             expect_time = datetime.today() - timedelta(hours=8)
             if published_date >= expect_time:
                 results.append({
-                    'title': soup.select('span.headline')[i].text,
-                    'url': 'https://tw.appledaily.com/' + \
-                        soup.select('a.story-card')[i].get('href'),
-                    'publisher': '蘋果新聞網',
+                    'title': title,
+                    'url': news_url,
+                    'publisher': '臺蘋新聞網',
                     'published_date': published_date
                 })
-            else:
-                break
+            # 臺蘋沒有按照時間排序 必須for完整個list
         return results
 
     async def get_setn_news(self, async_session, keyword):
         ''' get setn news by crawling'''
-        url = 'https://www.setn.com/search.aspx?q=' + \
-            urllib.parse.quote_plus(keyword) + '&r=0'
+        url = f'https://www.setn.com/search.aspx?q={urllib.parse.quote_plus(keyword)}&r=0'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         titles = soup.select('div.newsimg-area-text-2')
         url_tag = soup.select("div.newsimg-area-info >  a.gt ")
         dates = soup.select('div.newsimg-date')
-        #images = soup.select('img.lazy')
         results = []
         for value in enumerate(titles):
             date_string = dates[value[0]].text
-            #image = images[i].get('data-original').replace('-L','-PH')
             date_format = "%Y/%m/%d %H:%M"
             published_date = datetime.strptime(date_string, date_format)
             expect_time = datetime.today() - timedelta(hours=8)
@@ -132,23 +129,21 @@ class NewsCrawler:
 
     async def get_ettoday_news(self, async_session, keyword):
         ''' get udn ettoday by crawling'''
-        url = 'https://www.ettoday.net/news_search/doSearch.php?search_term_string=' + \
-            urllib.parse.quote_plus(keyword)
+        url = f'https://www.ettoday.net/news_search/doSearch.php?search_term_string=\
+            {urllib.parse.quote_plus(keyword)}'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         titles = soup.select('h2 > a')
         date = soup.select('span.date')
-        # images = soup.select('img')
         results = []
         for value in enumerate(titles):
             publish = date[value[0]].text.split('/')[1].replace(' ', '')
-            #image = 'https:' + images[i].get('src').replace('/b','/d')
             date_format = "%Y-%m-%d%H:%M)"
             published_date = datetime.strptime(publish, date_format)
             expect_time = datetime.today() - timedelta(hours=8)
             if published_date >= expect_time:
                 results.append({
-                    'title': titles[value[0]].text,
+                    'title': titles[value[0]].text.replace('\u3000', ' '),  # 將全形space取代為半形space
                     'url': titles[value[0]].get('href'),
                     'publisher': 'ETtoday新聞雲',
                     'published_date': published_date
@@ -159,8 +154,7 @@ class NewsCrawler:
 
     async def get_tvbs_news(self, async_session, keyword):
         ''' get TVBS news by crawling'''
-        url = 'https://news.tvbs.com.tw/news/searchresult/' + \
-            urllib.parse.quote_plus(keyword) + '/news'
+        url = f'https://news.tvbs.com.tw/news/searchresult/{urllib.parse.quote_plus(keyword)}/news'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         titles = soup.select('h2')
@@ -176,7 +170,7 @@ class NewsCrawler:
             expect_time = datetime.today() - timedelta(hours=8)
             if published_date >= expect_time:
                 results.append({
-                    'title': titles[value[0]].text,
+                    'title': titles[value[0]].text.replace('\u3000', ' '),  # 將全形space取代為半形space
                     'url': each_url,
                     'publisher': 'TVBS新聞網',
                     'published_date': published_date
@@ -187,16 +181,13 @@ class NewsCrawler:
 
     async def get_china_news(self, async_session, keyword):
         ''' get chinatime news by crawling'''
-        url = 'https://www.chinatimes.com/search/' + \
-            urllib.parse.quote_plus(keyword) + '?chdtv'
+        url = f'https://www.chinatimes.com/search/{urllib.parse.quote_plus(keyword)}?chdtv'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         titles = soup.select('h3 > a')
         dates = soup.select('time')
-        # images = soup.select('img.photo')
         results = []
         for value in enumerate(titles):
-            # image = images[value[0]].get('src')
             date_string = dates[value[0]].get('datetime')
             date_format = "%Y-%m-%d %H:%M"
             published_date = datetime.strptime(date_string, date_format)
@@ -214,17 +205,15 @@ class NewsCrawler:
 
     async def get_storm_news(self, async_session, keyword):
         ''' get 風傳媒 news by crawling'''
-        url = 'https://www.storm.mg/site-search/result?q=' + \
-            urllib.parse.quote_plus(keyword) + '&order=none&format=week'
+        url = f'https://www.storm.mg/site-search/result?q={urllib.parse.quote_plus(keyword)}\
+            &order=none&format=week'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         titles = soup.select('p.card_title')
         urls = soup.select('a.card_substance')
         publish_dates = soup.select('span.info_time')
-        # images = soup.select('img.card_img')
         results = []
         for value in enumerate(titles):
-            # image = images[i].get('src').replace('150x150','800x533')
             publish_date = publish_dates[value[0]].text
             date_format = "%Y-%m-%d %H:%M"
             published_date = datetime.strptime(publish_date, date_format)
@@ -242,19 +231,16 @@ class NewsCrawler:
 
     async def get_ttv_news(self, async_session, keyword):
         ''' get 台視 news by crawling'''
-        url = 'https://news.ttv.com.tw/search/' + \
-            urllib.parse.quote_plus(keyword)
+        url = f'https://news.ttv.com.tw/search/{urllib.parse.quote_plus(keyword)}'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         titles = soup.select('ul > li > a.clearfix > div.content > div.title')
         urls = soup.select('ul > li > a.clearfix')
         publishes = soup.select(
             'ul > li > a.clearfix > div.content > div.time')
-        # images = soup.select('img[alt=""]')
         results = []
         for value in enumerate(urls):
             publish = publishes[value[0]].text
-            # image = images[i].get('src')
             date_format = "%Y/%m/%d %H:%M:%S"
             published_date = datetime.strptime(publish, date_format)
             expect_time = datetime.today() - timedelta(hours=8)
@@ -271,18 +257,15 @@ class NewsCrawler:
 
     async def get_ftv_news(self, async_session, keyword):
         ''' get 民視 news by crawling'''
-        url = 'https://www.ftvnews.com.tw/search/' + \
-            urllib.parse.quote_plus(keyword)
+        url = f'https://www.ftvnews.com.tw/search/{urllib.parse.quote_plus(keyword)}'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         titles = soup.select('div.title')
         urls = soup.select('ul > li > a.clearfix')
         publishes = soup.select('div.time')
-        # images = soup.select('img[loading]')
         results = []
         for value in enumerate(urls):
             publish = publishes[value[0]].text
-            # image = images[i].get('src')
             date_format = "%Y/%m/%d %H:%M:%S"
             published_date = datetime.strptime(publish, date_format)
             expect_time = datetime.today() - timedelta(hours=8)
@@ -299,8 +282,7 @@ class NewsCrawler:
 
     async def get_cna_news(self, async_session, keyword):
         ''' get 中央社 news by crawling'''
-        url = 'https://www.cna.com.tw/search/hysearchws.aspx?q=' + \
-            urllib.parse.quote_plus(keyword)
+        url = f'https://www.cna.com.tw/search/hysearchws.aspx?q={urllib.parse.quote_plus(keyword)}'
         res = await async_session.get(url=url, headers=self.headers)
         soup = BeautifulSoup(res.text, 'html.parser')
         urls = soup.select('ul.mainList > li > a')
@@ -309,18 +291,13 @@ class NewsCrawler:
         results = []
         for value in enumerate(urls):
             publish = dates[value[0]].text
-            # image_url = urls[value[0]].img
-            # if image_url != None:
-            #   image = image_url['data-src'].replace('/200/','/400/')
-            # else:
-            #   image = None
             date_format = "%Y/%m/%d %H:%M"
             published_date = datetime.strptime(publish, date_format)
             expect_time = datetime.today() - timedelta(hours=8)
             if published_date >= expect_time:
                 results.append({
                     'title': titles[value[0]].text,
-                    'url': urls[value[0]].get('href'),
+                    'url': f'https://www.cna.com.tw/{urls[value[0]].get("href")}',
                     'publisher': 'CNA中央社',
                     'published_date': published_date
                 })
@@ -340,7 +317,8 @@ class NewsCrawler:
             each_url = value.get('href')
             res = await async_session.get(url=each_url, headers=self.headers)
             soup = BeautifulSoup(res.text, 'html.parser')
-            if 'health.ltn' in each_url or 'sports.ltn' in each_url or 'ec.ltn' in each_url:
+            if 'health.ltn' in each_url or 'sports.ltn' in each_url or \
+                'ec.ltn' in each_url or 'def.ltn' in each_url:
                 publish = soup.select('span.time')[1].text.replace(
                     '\n    ', '').replace('\r', '')
             elif 'ent.ltn' in each_url:
@@ -393,12 +371,12 @@ async def main(*keywords):
     cna_task = (NewsCrawler().get_cna_news(async_session, keyword)
                 for keyword in keywords)
     gather = await asyncio.gather(*udn_task, *apple_task, *setn_task, *ettoday_task
-            ,*tvbs_task,*china_task, *storm_task, *ttv_task, *ftv_task, *ltn_task, *cna_task)
+           ,*tvbs_task,*china_task, *storm_task, *ttv_task, *ftv_task, *ltn_task, *cna_task)
     return gather
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    result = asyncio.run(main('日本'))
+    result = asyncio.run(main('立法院'))
     print(result)
     end = time.perf_counter() - start
     print(end)
